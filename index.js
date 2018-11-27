f67319fbea1519bb1db6a0b3e1f4d6170a6bc641
Oliver Coley <mxoliverleigh@gmail.com>
2018-11-27 14:03:45 -0500

    edit initial layout add images directory

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.header').height($(window).height());
+})
+
+/*header style*/
+.header {
+  background-image: url('../images/header-bg.jpg');
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center;
+}
