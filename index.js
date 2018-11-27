0dd00e1dc9556b9064dc2991ee7cc4a0008ff8cd
Oliver Coley <mxoliverleigh@gmail.com>
2018-11-27 16:15:45 -0500

    upload images files and fix src

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
+/* website js code */
+
 $(document).ready(function(){
   $('.header').height($(window).height());
 })
-
-/*header style*/
-.header {
-  background-image: url('../images/header-bg.jpg');
-  background-attachment: fixed;
-  background-size: cover;
-  background-position: center;
-}
