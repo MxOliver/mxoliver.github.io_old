b45fab9bf0e8f4aa7500a598bfb6a46c4ed94c71
Oliver Coley <mxoliverleigh@gmail.com>
2018-11-30 16:05:58 -0500

    responsive vid script

--- index.js
+++ index.js
@@ -10,3 +10,34 @@ $(".navbar a").click(function(){
   },1000)
 
  })
+
+/* mobile optimized video dimensions */
+
+ var player_width = 854;
+ var player_height = 480;
+
+ var responsiveVideo = function() {
+
+ //see note below
+  var player_window_buffer = 120;
+  var window_width = $(window).width();
+  var aspect_ratio = player_width/player_height;
+  var player_span = $('span.vidyard_player > span');
+  var player_iframe = $('iframe.vidyard_iframe');
+
+  if (window_width >= player_width) {
+   $(player_span).width(player_width)
+   $(player_span).height(player_height).css('line-height', player_height + 'px');
+   $(player_iframe).width(player_width);
+   $(player_iframe).height(player_height);
+  } else {
+   $(player_span).width(window_width-player_window_buffer);
+   var new_wrapper_height = $(player_span).width()/aspect_ratio;
+   $(player_span).height(new_wrapper_height).css('line-height', new_wrapper_height + 'px');
+   $(player_iframe).width(window_width-player_window_buffer);
+   $(player_iframe).height(($(player_span).width()/aspect_ratio));
+  }
+ }
+
+ $(document).ready(function(){responsiveVideo();});
+ $(window).resize(function() {responsiveVideo();});
