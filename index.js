b7a646a67030249416363534639fca77bab55c10
Oliver Coley <mxoliverleigh@gmail.com>
2019-01-25 14:09:43 -0500

    js file cleanup

--- index.js
+++ index.js
@@ -13,56 +13,6 @@ $(".navbar a").click(function() {
 
 })
 
-$('.collapse').collapse()
+/* toggle */
 
-/* mobile optimized video dimensions */
-
-var player_width = 854;
-var player_height = 480;
-
-var responsiveVideo = function() {
-
-    //see note below
-    var player_window_buffer = 120;
-    var window_width = $(window).width();
-    var aspect_ratio = player_width / player_height;
-    var player_span = $('span.vidyard_player > span');
-    var player_iframe = $('iframe.vidyard_iframe');
-
-    if (window_width >= player_width) {
-        $(player_span).width(player_width)
-        $(player_span).height(player_height).css('line-height', player_height + 'px');
-        $(player_iframe).width(player_width);
-        $(player_iframe).height(player_height);
-    } else {
-        $(player_span).width(window_width - player_window_buffer);
-        var new_wrapper_height = $(player_span).width() / aspect_ratio;
-        $(player_span).height(new_wrapper_height).css('line-height', new_wrapper_height + 'px');
-        $(player_iframe).width(window_width - player_window_buffer);
-        $(player_iframe).height(($(player_span).width() / aspect_ratio));
-    }
-}
-
-$(document).ready(function() { responsiveVideo(); });
-$(window).resize(function() { responsiveVideo(); });
-
-
-/* progress bars */
-
-/* 
-
-var ProgressBar = require('progressbar.js')
-
-var bar = new ProgressBar.Circle('.skills-bar #container', {
-    strokeWidth: 6,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#FFEA82',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: null
-});
-
-bar.animate(1.0); // Number from 0.0 to 1.0 
-
-*/
+$('.collapse').collapse('toggle');
